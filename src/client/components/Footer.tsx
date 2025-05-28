109d2052ed7f2cd715072c06f3d4d9fbbd88356b
abduldoesramen <abdulrehman.ahmad@qantasloyalty.com>
2025-05-28 12:37:32 +1000

    add footer icons

--- src/client/components/Footer.tsx
+++ src/client/components/Footer.tsx
@@ -1,6 +1,9 @@
 import { FC, ReactElement } from "react";
 import { Box, Container, Typography } from "@mui/material";
 import Grid from "@mui/material/Grid";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import EmailIcon from '@mui/icons-material/Email';
 
 const linkedInUrl = "https://www.linkedin.com/in/abdulrehman-ahmad-76b9a0223"
 const githubUrl = "https://github.com/abduldoesramen"
@@ -24,17 +27,20 @@ export const Footer: FC = (): ReactElement => {
             </Typography>
           </Grid>
           <Grid container direction="row" alignItems="center"  color="beige" spacing={2}>
-            <Grid>
+            <Grid container direction="row" alignItems="center" spacing={1}>
+              <EmailIcon/>
               <Typography variant="subtitle2">
                 <div>abdulrehman133782@gmail.com</div>
               </Typography>
             </Grid>
-            <Grid>
+            <Grid container direction="row" alignItems="center" spacing={1}>
+              <LinkedInIcon/>
               <Typography variant="subtitle2">
                 <a href={linkedInUrl} target="_blank" rel="noopener noreferrer" style={{color: "beige"}}>LinkedIn</a>
               </Typography>
             </Grid>
-            <Grid>
+            <Grid container direction="row" alignItems="center" spacing={1}>
+              <GitHubIcon/>
               <Typography variant="subtitle2">
                 <a href={githubUrl} target="_blank" rel="noopener noreferrer" style={{color: "beige"}}>Github</a>
               </Typography>
